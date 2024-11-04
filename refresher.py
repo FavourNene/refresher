02428194e6705af155ed8e0d3ca06f01d0f16791
FavourNene <113694679+FavourNene@users.noreply.github.com>
2024-11-04 15:21:55 +0100

    Update refresher.py

--- refresher.py
+++ refresher.py
@@ -1,6 +1,7 @@
-fibonacci_sequence = [0 ,1]
-print(fibonacci_sequence)
-next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-print(next_num)
-
-
+fibonacci_sequence = [0, 1]
+while len(fibonacci_sequence) < 51:
+    next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+    fibonacci_sequence.append(next_num)
+print("THIS IS A LIST OF THE FIRST 50 NUMBERS IN THE FIBONACCI SEQUENCE:")
+for i in fibonacci_sequence:
+    print(str(i) + '\n')
